20f258649e469f4e3df9f9fff068261117c4bb8d
Mukul Vasisht <his-email@example.com>
2025-05-29 22:21:34 +0530

    Initial commit

---
+++ ClassTeacher/Includes/footer.php
@@ -0,0 +1,8 @@
+<footer class="sticky-footer bg-white" style="position: fixed;bottom: 0px;width: 100%;">
+        <div class="container my-auto" style="margin: auto calc(50% - 44rem) !important;">
+          <div class="copyright text-center my-auto">
+            <span> &copy; <script> document.write(new Date().getFullYear()); </script> 
+            </span>
+          </div>
+        </div>
+      </footer>
